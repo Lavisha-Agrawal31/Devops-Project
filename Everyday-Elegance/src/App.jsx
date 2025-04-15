6ffe1172fa55048108a30901f8d40442f89c1cbc
Lavisha-Agrawal31 <laviagrawal3112@gmail.com>
2025-04-15 17:59:37 +0530

    Added DockerFile and Compose File

---
+++ Everyday-Elegance/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import ProductDetails from "./components/ProductDetails";
+import ThankYou from "./components/ThankYou";
+import Products from "./components/Products";
+import SearchResults from "./components/Navbar";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/productDetails/:productId" element={<ProductDetails />} />
+        <Route path="/thank-you" element={<ThankYou />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/search" element={<SearchResults />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
